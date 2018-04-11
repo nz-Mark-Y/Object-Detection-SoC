b431af62a3ec231ea415a802e1ea300d66abfb97
சகாயன் சிற்சபேசன் <ssit662@aucklanduni.ac.nz>
2018-04-11 12:04:06 +1200

    Updating to Phase 1 ZIP

--- mjpeg/my_trivium.h
+++ mjpeg/my_trivium.h
@@ -5,7 +5,10 @@
 #define MAXIVSIZEB ((ECRYPT_MAXIVSIZE + 7) / 8)
 #include "ecrypt-sync.h"
 
-int trivium_decrypt_file(char* infile, char* outfile);
+int trivium_decrypt_file(char* infile, char* outfile, const u8 key[], const u8 iv[]);
 int trivium_test_cipher(void);
+
+void print_key(const u8 key[]);
+void print_iv(const u8 iv[]);
 
 #endif /* MY_TRIVIUM_H */
